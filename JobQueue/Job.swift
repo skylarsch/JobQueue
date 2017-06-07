a82584e694cfd31d1dc79f33f595492573bb8e2f
Skylar Schipper <ss@schipp.co>
2017-06-06 19:54:54 -0700

    Initial commit

---
+++ JobQueue/Job.swift
@@ -0,0 +1,12 @@
+//
+//  Job.swift
+//  JobQueue
+//
+//  Created by Skylar Schipper on 6/6/17.
+//  Copyright © 2017 Skylar Schipper. All rights reserved.
+//
+
+import Foundation
+
+open class Job : Operation {
+}
---
+++ JobQueue/JobQueue.h
@@ -0,0 +1,8 @@
+//
+//  JobQueue.h
+//  JobQueue
+//
+//  Created by Skylar Schipper on 6/6/17.
+//  Copyright © 2017 Skylar Schipper. All rights reserved.
+//
+
